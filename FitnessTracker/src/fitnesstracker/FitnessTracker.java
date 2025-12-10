e3a1f9d8d86ed0586fd2f303290e266e2f2181c5
arfraeeel1 <20241506@nbsc.edu.ph>
2025-12-10 17:13:53 +0800

    Second Commit contains sign up and log in menu

--- FitnessTracker/src/fitnesstracker/FitnessTracker.java
+++ FitnessTracker/src/fitnesstracker/FitnessTracker.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 
 
-public class Part1 {
+public class FitnessTracker {
 
     private static Map<String, String> users = new HashMap<>();
     private static Map<String, StringBuilder> sessionHistory = new HashMap<>();
@@ -27,6 +27,69 @@ public class Part1 {
             } else {
                 mainMenu();
             }
+        }
+    }
+ private static void authMenu() {
+
+        String menu = """
+                ------------------------------
+                        FITNESS TRACKER
+                ------------------------------
+
+                [1] Sign Up
+                [2] Log In
+                [3] Exit
+
+                Select option:
+                """;
+
+        String input = JOptionPane.showInputDialog(null, menu);
+
+        if (input == null) return;
+
+        switch (input) {
+            case "1" -> signUp();
+            case "2" -> login();
+            case "3" -> System.exit(0);
+            default -> JOptionPane.showMessageDialog(null, "Invalid choice.");
+        }
+    }
+
+    protected static void signUp() {
+        String user = JOptionPane.showInputDialog("Enter new username:");
+
+        if (user == null || user.isEmpty()) return;
+
+        if (Part1.users.containsKey(user)) {
+            JOptionPane.showMessageDialog(null, "Username already exists.");
+            return;
+        }
+
+        String pass = JOptionPane.showInputDialog("Enter password:");
+
+        if (pass == null || pass.isEmpty()) return;
+
+        Part1.users.put(user, pass);
+        Part1.sessionHistory.put(user, new StringBuilder());
+
+        JOptionPane.showMessageDialog(null, "Account created!");
+    }
+
+    protected static void login() {
+        String user = JOptionPane.showInputDialog("Username:");
+
+        if (user == null) return;
+
+        String pass = JOptionPane.showInputDialog("Password:");
+
+        if (pass == null) return;
+
+        if (Part1.users.containsKey(user) && Part1.users.get(user).equals(pass)) {
+            Part1.loggedIn = true;
+            Part1.currentUser = user;
+            JOptionPane.showMessageDialog(null, "Welcome, " + user + "!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Invalid login.");
         }
     }
 }
